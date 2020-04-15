332c95e59399f2a9846710aa5a786494862b5fe1
Kenny-Lacroix <62295602+Kenny-Lacroix@users.noreply.github.com>
2020-04-15 20:38:58 +0200

    added links between sing up & sign in

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,17 +5,17 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="background-gray" >
-                <h1 class="display-4">Welcome,</h1>
-                <p class="lead">To use our service please login.</p>
-                <p class="lead">
-                    <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>
-                </p>
-                <hr class="my-4">
+                <h1 class="display-4">Welcome to ParkIt</h1>
                 <h3 class="display-6" class="lead">What do we offer?</h3>
                 <p>We offer a service that makes it possible to hire or lease a parking spot.
                     <div class="row justify-content-center">
                         <img src="images/parking.jpg" alt="parking image">
                     </div>
+                </p>
+                <hr class="my-4">
+                <p class="lead">To use our service you are required to have an account</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="/register" role="button">Sign up</a>
                 </p>
             </div>
         </div>
