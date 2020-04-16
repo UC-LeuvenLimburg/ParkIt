d99d95f20644e8f489acbd0867af6a1cc97e540b
Kenny-Lacroix <62295602+Kenny-Lacroix@users.noreply.github.com>
2020-04-16 22:23:53 +0200

    userSeeder factory

--- database/seeds/userSeeder.php
+++ database/seeds/userSeeder.php
@@ -14,8 +14,19 @@ class userSeeder extends Seeder
             DB::table('users')->insert([
             'name' => 'John Doe',
             'email' =>'John.Doe@hotmail.com' ,
+            'email_verified_at' => now(),
             'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
         ]);
+        factory(App\Models\User::class, 50)->create();
     }
+
+            //     $table->id();
+            // $table->string('name');
+            // $table->string('email')->unique();
+            // $table->timestamp('email_verified_at')->nullable();
+            // $table->string('password');
+            // $table->rememberToken();
+            // $table->timestamps();
     
 }
