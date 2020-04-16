b3d0ebb96e8a76b86cd430cef2e39fd2f33ff7cb
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-04-16 18:53:53 +0200

    Added lease repositories

---
+++ app/Repositories/LeaseRepository.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Lease;
+use Illuminate\Database\Eloquent\Collection;
+use App\Repositories\Interfaces\ILeaseRepository;
+
+class LeaseRepository implements ILeaseRepository
+{
+    /**
+     * Get's all leases
+     *
+     * @return Illuminate\Database\Eloquent\Collection
+     */
+    public function getLeases(): Collection
+    {
+        return Lease::all();
+    }
+
+    /**
+     * Get's a lease by it's ID
+     *
+     * @param int
+     * @return \App\Lease
+     */
+    public function getLease(int $lease_id): ?Lease
+    {
+        return Lease::find($lease_id);
+    }
+
+    /**
+     * Add a lease
+     *
+     * @param lease
+     * @return \App\Lease
+     */
+    public function addLease(Lease $lease): Lease
+    {
+        // Add lease to database
+        $lease::save();
+
+        return $lease;
+    }
+
+    /**
+     * Remove a lease by it's ID
+     *
+     * @param int
+     */
+    public function deleteLease(int $lease_id): void
+    {
+        Lease::destroy($lease_id);
+    }
+}
