e43fe1ef440658c1c2d4d42bb0aa822a83c5f238
ChaosShay <ChaosShay@outlook.com>
2020-04-16 15:57:46 +0200

    Creating the relations

--- app/User.php
+++ app/User.php
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function rentable() {
+        return $this->hasMany('App\Rentable');
+    }
+
+    public function lease() {
+        return $this->hasMany('App\Lease');
+    }
 }
