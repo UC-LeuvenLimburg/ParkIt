20b02d94ed7f07a6a6ca837204faf52f3a507aa0
kaiye13 <kaatschillemans@hotmail.com>
2020-04-16 15:50:20 +0200

    adjuted rentable.php , added fillable and hidden attributes

--- app/Rentable.php
+++ app/Rentable.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rentable extends Model
 {
-    //
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'rent_place_id', 'owner_id', 'postal_code', 'address','date_of_hire', 'start_time_rp','end_time_rp','price/h','bankaccount_nr','description'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'bankaccount_nr',
+    ];
+
+
 }
