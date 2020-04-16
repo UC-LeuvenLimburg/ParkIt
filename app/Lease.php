3821ba12e7bf2a4de7c2144b2e6f727e90716bea
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-04-16 15:35:41 +0200

    added fields to lease model

--- app/Lease.php
+++ app/Lease.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lease extends Model
 {
-
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'startTime', 'endTime',
+    ];
 }
