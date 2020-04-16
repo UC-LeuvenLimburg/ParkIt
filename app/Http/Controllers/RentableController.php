7248ca90061582f3fc7cf089d898e10b1a7fff8e
kaiye13 <kaatschillemans@hotmail.com>
2020-04-16 15:33:18 +0200

    created rentableModel

---
+++ app/Http/Controllers/RentableController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Rentable;
+use Illuminate\Http\Request;
+
+class RentableController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Rentable  $rentable
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Rentable $rentable)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Rentable  $rentable
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Rentable $rentable)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Rentable  $rentable
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Rentable $rentable)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Rentable  $rentable
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Rentable $rentable)
+    {
+        //
+    }
+}
