082e2697b3f05333afe45d908c3a15157d4e7778
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-04-17 13:19:22 +0200

    added user role to database seeder added admin role to admin user

--- database/seeds/userSeeder.php
+++ database/seeds/userSeeder.php
@@ -11,14 +11,14 @@ class userSeeder extends Seeder
      */
     public function run()
     {
-            DB::table('users')->insert([
+        DB::table('users')->insert([
             'name' => 'admin',
-            'email' =>'admin@gmail.com' ,
+            'email' => 'admin@gmail.com',
             'email_verified_at' => now(),
             'password' => Hash::make('admin'),
+            'role' => 'admin',
             'remember_token' => Str::random(10),
         ]);
         factory(App\Models\User::class, 100)->create();
     }
-    
 }
