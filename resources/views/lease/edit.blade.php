0374b4433deaf513d944be0173e7ff589f9345cf
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-04-18 23:34:32 +0200

    completed lease edit form

--- resources/views/lease/edit.blade.php
+++ resources/views/lease/edit.blade.php
@@ -5,7 +5,15 @@
     <div class="row justify-content-center">
         <div class="col-xl">
             <h1>Edit Lease</h1>
-            {!! Form::model($lease, ['route' => ['leases.update', $lease->id]]) !!}
+            {!! Form::model($lease, ['route' => ['leases.update', $lease]]) !!}
+            <div class="form-group">
+                {{Form::label('adress', 'Adress')}}
+                {{Form::text('adress', $lease->rentable->adress, ['class' => 'form-control', 'placeholer' => 'Adress', 'disabled']) }}
+            </div>
+            <div class="form-group">
+                {{Form::label('date', 'Date')}}
+                {{Form::date('date', $lease->rentable->date_of_hire, ['class' => 'form-control', 'placeholer' => 'Date', 'disabled']) }}
+            </div>
             <div class="form-group">
                 {{Form::label('start_time', 'Start Time')}}
                 {{Form::time('start_time', $lease->start_time, ['class' => 'form-control', 'placeholer' => 'Start Time']) }}
@@ -14,6 +22,7 @@
                 {{Form::label('end_time', 'End Time')}}
                 {{Form::time('end_time', $lease->end_time, ['class' => 'form-control', 'placeholer' => 'End Time']) }}
             </div>
+            {{Form::submit('Save', [ 'class' => 'btn btn-primary'])}}
             {!! Form::close() !!}
         </div>
     </div>
