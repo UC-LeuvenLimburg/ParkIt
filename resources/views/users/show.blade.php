43ab4479f85c13e1c7ed0b1cce82e9971d20aca0
Kenny-Lacroix <62295602+Kenny-Lacroix@users.noreply.github.com>
2020-04-19 20:36:55 +0200

    changed layout

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,18 +4,18 @@
 <div class="container">
     <h1>Show User</h1>
     <div class="card border-primary">
-        <h4 class="card-header">User Id: {{ $user->id }}</h4>
+        <h4 class="card-header">Name: {{ $user->name }}</h4>
         <div class="card-body">
-            Name: {{ $user->name }}<br>
+            User Id: {{ $user->id }}<br>
             Email: {{ $user->email }}<br>
             Role: {{ $user->role }}<br>
             Email verified at: {{ $user->email_verified_at }}<br>
             Created at: {{ $user->created_at }}
             @if ($user->name != 'admin' /*&& $current_user->isAdmin()*/)
-                {!! Form::open(['action' => ['UserController@destroy', $user], 'method' => 'POST']) !!}
-                    {{ Form::hidden('_method', 'DELETE') }}
-                    {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
-                {!! Form::close() !!}
+            {!! Form::open(['action' => ['UserController@destroy', $user], 'method' => 'POST']) !!}
+            {{ Form::hidden('_method', 'DELETE') }}
+            {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
+            {!! Form::close() !!}
             @endif
         </div>
     </div>
