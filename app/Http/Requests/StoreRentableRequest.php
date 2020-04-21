6004430e82678d16a01dcf696aa98e7eb5e6255d
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-04-21 23:29:50 +0200

    added rentable fields to rule set

--- app/Http/Requests/StoreRentableRequest.php
+++ app/Http/Requests/StoreRentableRequest.php
@@ -7,16 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class StoreRentableRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
@@ -24,7 +14,26 @@ class StoreRentableRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'user_id' => 'required|integer|exists:App\Models\User,id',
+            'postal_code' => 'required|digits:4|min:0001',
+            'adress' => 'required|string|min:2',
+            'date_of_hire' => 'required',
+            'start_time' => 'required',
+            'end_time' => 'required',
+            'price' => 'required|numeric',
+            'bankaccount_nr' => 'required',
+            'description' => 'required',
         ];
+    }
+
+    /**
+     * Validate request
+     * @return Illuminate\Foundation\Http\FormRequest::getValidatorInstance
+     */
+    protected function getValidatorInstance()
+    {
+        return parent::getValidatorInstance()->after(function () {
+            // todo custom validation for rentable
+        });
     }
 }
