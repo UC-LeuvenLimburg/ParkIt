f211d8296509314d111217493a99c8264be19bae
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-04-21 23:53:47 +0200

    added maximum constraints to validation rules

--- app/Http/Requests/StoreRentableRequest.php
+++ app/Http/Requests/StoreRentableRequest.php
@@ -15,12 +15,12 @@ class StoreRentableRequest extends FormRequest
     {
         return [
             'user_id' => 'required|integer|min:1|exists:App\Models\User,id',
-            'adress' => 'required|string|min:2',
-            'postal_code' => 'required|digits:4|min:0001|max:9999',
-            'date_of_hire' => 'required|date_format:d-m-Y',
+            'adress' => 'required|string|min:3|max:255',
+            'postal_code' => 'required|numeric|digits:4|min:1|max:9999',
+            'date_of_hire' => 'required|date_format:Y-m-d',
             'start_time' => 'required',
             'end_time' => 'required',
-            'price' => 'required|numeric',
+            'price' => 'required|numeric|min:0.01|max:1000',
             'bankaccount_nr' => 'required',
             'description' => 'required',
         ];
