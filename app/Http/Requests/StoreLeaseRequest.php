2eb6fe461f855892fe4c1d872d6d9a680a8b3f3b
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-04-21 20:58:40 +0200

    added time available check to lease form validator

--- app/Http/Requests/StoreLeaseRequest.php
+++ app/Http/Requests/StoreLeaseRequest.php
@@ -24,29 +24,45 @@ class StoreLeaseRequest extends FormRequest
 
     /**
      * Validate request
-     * @return
+     * @return Illuminate\Foundation\Http\FormRequest::getValidatorInstance
      */
     protected function getValidatorInstance()
     {
         return parent::getValidatorInstance()->after(function () {
+            // Get the asociated rentable
+            $rentable = Rentable::find($this->input('rentable_id'));
+            // convert to unix timestamps
+            $lease_start_time = strtotime($this->input('start_time'));
+            $lease_end_time = strtotime($this->input('end_time'));
+            $rentable_start_time = strtotime($rentable->start_time);
+            $rentable_end_time = strtotime($rentable->end_time);
+
             // Check if start_time is before end_time
-            if ($this->input('start_time') >= $this->input('end_time')) {
-                $this->validator->errors()->add('end_time', 'End Time cannot be before Start Time!');
+            if ($lease_start_time >= $lease_end_time) {
+                $this->validator->errors()->add('end_time', 'End time cannot be before start time!');
             }
 
             // Check if minimum rented time is greater then 30 minutes
-            if ($this->timeDiffInMinutes($this->input('start_time'), $this->input('end_time')) < 30) {
-                $this->validator->errors()->add('end_time', 'At least 30 minutes have to be rented!');
+            if ($this->timeDiffInMinutes($lease_start_time, $lease_end_time) < 30) {
+                $this->validator->errors()->add('rented_time', 'At least 30 minutes have to be rented!');
+            }
+
+            // Check if lease time is available on rentable
+
+            // Check if start_time lease is before start_time rentable
+            if ($lease_start_time < $rentable_start_time) {
+                $this->validator->errors()->add('available_time', 'Your current start time is before the available start time!');
+            }
+
+            // Check if end_time lease is after end_time rentable
+            if ($lease_end_time > $rentable_end_time) {
+                $this->validator->errors()->add('available_time', 'Your current end time is after the available end time!');
             }
         });
     }
 
     protected function timeDiffInMinutes($firstTime, $lastTime)
     {
-        // convert to unix timestamps
-        $firstTime = strtotime($firstTime);
-        $lastTime = strtotime($lastTime);
-
         // perform subtraction to get the difference (in seconds) between times
         $timeDiff = ($lastTime - $firstTime) / 60;
 
