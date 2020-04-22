b400d81b92968f474270cd0381c5658bba252982
Kenny-Lacroix <62295602+Kenny-Lacroix@users.noreply.github.com>
2020-04-22 22:04:15 +0200

    added password editing

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -4,7 +4,7 @@
     <div class="row justify-content-center">
         <div class="col-xl">
             <h1>User details</h1>
-            {!! Form::model($user) !!}
+           {!! Form::model($user, ['route' => ['users.update', $user], 'method' => 'PUT']) !!}
             <div class="form-group">
                 {{Form::label('name', 'Name')}}
                 {{Form::text('name', $user->name, ['class' => 'form-control', 'placeholer' => 'Name', 'disabled']) }}
@@ -14,13 +14,10 @@
                 {{Form::email('email', $user->email, ['class' => 'form-control', 'placeholer' => 'Email', 'disabled']) }}
             </div>
             <div class="form-group">
-                {{Form::label('password', 'Password')}}
+                {{Form::label('password', 'New Password')}}
                 {{Form::password('password', ['class' => 'form-control', 'placeholer' => 'Password']) }}
             </div>
-            <div class="form-group">
-                {{Form::label('password', 'Confirm New Password')}}
-                {{Form::password('password', ['class' => 'form-control', 'placeholer' => 'Password']) }}
-            </div>
+            {{Form::submit('change password', [ 'class' => 'btn btn-primary'])}}
             {!! Form::close() !!}
         </div>
     </div>
