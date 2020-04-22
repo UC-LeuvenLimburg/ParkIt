c261bf39b616525ffdda8f7323fed33a9815518f
Kenny-Lacroix <62295602+Kenny-Lacroix@users.noreply.github.com>
2020-04-22 20:40:28 +0200

    changed links in nav

--- resources/views/include/nav.blade.php
+++ resources/views/include/nav.blade.php
@@ -56,8 +56,8 @@
 
                                 {{-- These are USER only --}}
                                 @if (Auth::user()->role==="user")
-                                <a class="dropdown-item" href="{{action('LeaseController@myleases')}}">My Leases</a>
-                                <a class="dropdown-item" href="{{action('RentableController@myplaces')}}">My Places</a>
+                                <a class="dropdown-item" href="{{action('LeaseController@index')}}">My Leases</a>
+                                <a class="dropdown-item" href="{{action('RentableController@index')}}">My Places</a>
                                 @endif
                                 {{--  --}}
 
