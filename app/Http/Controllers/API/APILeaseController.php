eb4b43d684e27638b7d3cddb0794d0818f24b3e8
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-04-25 17:00:39 +0200

    created API Lease controller

---
+++ app/Http/Controllers/API/APILeaseController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Lease;
+use App\Models\Rentable;
+use App\Repositories\Interfaces\ILeaseRepository;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class APILeaseController extends Controller
+{
+    private $leaseRepo;
+
+    public function __construct(ILeaseRepository $leaseRepo)
+    {
+        $this->authorizeResource(Lease::class, 'lease');
+        $this->leaseRepo = $leaseRepo;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $leases =  $this->leaseRepo->getLeases();
+        return view('lease.index', compact('leases'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $user = Auth::user();
+        $rentable = Rentable::find(1);
+        return view('lease.create')->with(compact('user', 'rentable'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $newLease = $this->leaseRepo->addLease($request->all());
+        return redirect('/leases/' . $newLease->id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Lease $lease
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Lease $lease)
+    {
+        return view('lease.show', compact('lease'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Lease $lease
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Lease $lease)
+    {
+        return view('lease.edit', compact('lease'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Models\Lease $lease
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Lease $lease)
+    {
+        $this->leaseRepo->updateLease($lease->id, $request->all());
+        return redirect('/leases/' . $lease->id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Lease $lease
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lease $lease)
+    {
+        $this->leaseRepo->deleteLease($lease->id);
+        return redirect('/leases')->with('success', 'Lease Removed');
+    }
+
+    /**
+     * Display the specified leases
+     *
+     * @param  App\Models\Lease $lease
+     * @return \Illuminate\Http\Response
+     */
+    public function myleases()
+    {
+        $user = Auth::user();
+        //$leases =  $this->leasesRepo->getUserLeases($user);
+        return view(
+            'lease.myleases'
+            //, compact('leases')
+        );
+    }
+}
