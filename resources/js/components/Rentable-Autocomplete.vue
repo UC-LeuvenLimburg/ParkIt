ac91e3d8245fad21236f35ef8f54a9daffece894
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-04-25 21:29:50 +0200

    finished rentable autocomplete styling

--- resources/js/components/Rentable-Autocomplete.vue
+++ resources/js/components/Rentable-Autocomplete.vue
@@ -17,24 +17,28 @@
             @selected="rentableSelected"
         />
         <div class="filters">
-            <label for="date_of_hire">Date Filter:</label>
-            <input
-                type="date"
-                id="date_of_hire"
-                name="date_of_hire"
-                :value="date_of_hire"
-                class="form-control"
-                placeholder="Date"
-            />
-            <label for="postal_code">Postal Code Filter:</label>
-            <input
-                type="number"
-                id="postal_code"
-                name="postal_code"
-                :value="postal_code"
-                class="form-control"
-                placeholder="Postal Code"
-            />
+            <div class="filter-pair">
+                <label for="date_of_hire">Date Filter:</label>
+                <input
+                    type="date"
+                    id="date_of_hire"
+                    name="date_of_hire"
+                    :value="date_of_hire"
+                    class="form-control"
+                    placeholder="Date"
+                />
+            </div>
+            <div class="filter-pair no-margin-right">
+                <label for="postal_code">Postal Code Filter:</label>
+                <input
+                    type="text"
+                    id="postal_code"
+                    name="postal_code"
+                    :value="postal_code"
+                    class="form-control"
+                    placeholder="Postal Code"
+                />
+            </div>
         </div>
     </div>
 </template>
@@ -47,7 +51,7 @@ export default {
             rentables: [],
             rentable_id: 0,
             date_of_hire: null,
-            postal_code: 0
+            postal_code: ""
         };
     },
     mounted() {
@@ -71,6 +75,8 @@ export default {
 
 <style scoped>
 .rentable-form {
+    width: 100%;
+    position: relative;
     display: flex;
     flex-direction: row;
     justify-content: flex-start;
@@ -81,10 +87,24 @@ export default {
     flex-direction: inherit;
     justify-content: inherit;
     align-items: inherit;
-    min-width: 50%;
+    margin-left: 0.5em;
 }
-.filters label {
-    white-space: nowrap;
+.filter-pair {
+    position: relative;
     margin: 0 0.5em;
+}
+.no-margin-right {
+    margin-right: 0;
+}
+.filters .filter-pair label {
+    position: absolute;
+    left: 0;
+    bottom: 100%;
+    z-index: 999;
+    white-space: nowrap;
+}
+.filters .filter-pair input {
+    height: 40px;
+    min-width: 150px;
 }
 </style>
