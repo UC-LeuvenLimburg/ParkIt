ae17518247664b47c6982d77e9ffa2a2cf2d8771
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-04-25 18:29:46 +0200

    removed example component

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,10 +20,6 @@ window.Vue = require("vue");
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component(
-    "example-component",
-    require("./components/ExampleComponent.vue").default
-);
-Vue.component(
     "user-autocomplete",
     require("./components/User-Autocomplete.vue").default
 );
