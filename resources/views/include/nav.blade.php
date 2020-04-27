7ae446e42c38072b444e96620134cf87fd2811ef
ChaosShay <ChaosShay@outlook.com>
2020-04-27 12:58:19 +0200

    Login and register added

--- resources/views/include/nav.blade.php
+++ resources/views/include/nav.blade.php
@@ -27,11 +27,11 @@
             <ul class="navbar-nav ml-auto">
                 <!-- Authentication Links -->
                 @guest
-                <li class="nav-item">
+                <li class="nav-item {{ Request::is('login') ? 'active' : '' }}">
                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                 </li>
                 @if (Route::has('register'))
-                <li class="nav-item">
+                <li class="nav-item {{ Request::is('register') ? 'active' : '' }}">
                     <a class="nav-link" href="{{ route('register') }}">{{ __('Sign up') }}</a>
                 </li>
                 @endif
