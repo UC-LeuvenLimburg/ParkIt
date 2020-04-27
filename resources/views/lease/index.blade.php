8b81739f2068bc33094455eecd13bb4f89f7346f
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-04-28 00:05:38 +0200

    added available leases to rent page moved leases table to separate blade file

--- resources/views/lease/index.blade.php
+++ resources/views/lease/index.blade.php
@@ -6,37 +6,7 @@
     <h1>Leases</h1>
 
     @if (count($leases) > 0)
-    <table class="table table-dark table-hover">
-        <thead class="table-primary">
-            <tr>
-                <th scope="col">Lease</th>
-                <th scope="col">Date</th>
-                <th scope="col">Start time</th>
-                <th scope="col">End time</th>
-                <th scope="col">Phone Number</th>
-                <th scope="col">License Plate</th>
-                <th scope="col">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($leases as $lease)
-            <tr>
-                <td>{{$lease->rentable->adress}}</td>
-                <td>{{$lease->rentable->date_of_hire}}</td>
-                <td>{{$lease->start_time}}</td>
-                <td>{{$lease->end_time}}</td>
-                <td>{{$lease->phone_nr}}</td>
-                <td>{{$lease->license_plate}}</td>
-                <td>
-                    <a class="btn btn-info btn-sm" href='/leases/{{ $lease->id }}'>Show</a>
-                    @if (Auth::user()->role==="admin")
-                    <a class="btn btn-info btn-sm btn-warning" href='/leases/{{ $lease->id }}/edit'>Edit</a>
-                    @endif
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    @include('lease.table', $leases)
     <div class="parkit-paginator">
         {{ $leases->links() }}
         <div class="filler"></div>
