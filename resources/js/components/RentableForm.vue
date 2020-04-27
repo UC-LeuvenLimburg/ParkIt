54bdd4158a6d851cdb5efc5a88588faa2d771cb3
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-04-27 21:48:38 +0200

    added rentable form.vue

---
+++ resources/js/components/RentableForm.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <rentable-autocomplete />
+    </div>
+</template>
+
+<script>
+import RentableAutocomplete from "./RentableAutocomplete";
+
+export default {
+    components: {
+        RentableAutocomplete
+    }
+};
+</script>
+
+<style></style>
