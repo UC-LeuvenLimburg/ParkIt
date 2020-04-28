2b1afe84648ceeb4e617b73204af689afd73ac6d
Kenny-Lacroix <62295602+Kenny-Lacroix@users.noreply.github.com>
2020-04-28 02:23:17 +0200

    fixed typos

--- resources/views/payment/form.blade.php
+++ resources/views/payment/form.blade.php
@@ -4,19 +4,19 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-xl">
-            <h1>Payment form</h1>
+            <h1>Payment</h1>
             {!! Form::open(['url' => '/pay']) !!}
             <div class="form-group">
                 {{Form::label('price', 'Price')}}
                 {{Form::text('price', $totalPrice, ['class' => 'form-control', 'placeholer' => 'Price', 'disabled']) }}
             </div>
             <div class="form-group">
-                {{Form::label('name', 'Name')}}
+                {{Form::label('name', 'Owner')}}
                 {{Form::text('name',$lease->rentable->user->name, ['class' => 'form-control', 'placeholer' => 'name', 'disabled']) }}
             </div>
             <div class="form-group">
                 {{Form::label('bankaccount_nr', 'Bankaccount Number')}}
-                {{Form::text('bankaccount_nr', $lease->rentable->bankaccount_nr, ['class' => 'form-control', 'placeholer' => 'Bankaccount number', 'required'])}}
+                {{Form::text('bankaccount_nr', $lease->rentable->bankaccount_nr, ['class' => 'form-control', 'placeholer' => 'Bankaccount number', 'disabled'])}}
             </div>
             {{Form::submit('Pay', [ 'class' => 'btn btn-primary'])}}
             <a href="javascript:history.back()" class="btn btn-primary">Back</a>
