b4d100aa086bc0bba4eea6109ec94d3ba82fac5e
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-05-04 23:47:47 +0200

    fixed database seeder so times make sence

--- database/factories/leaseFactory.php
+++ database/factories/leaseFactory.php
@@ -10,8 +10,8 @@ $factory->define(Lease::class, function (Faker $faker) {
     return [
         'user_id' => $faker->numberBetween(1, 50),
         'rentable_id' => $faker->numberBetween(1, 50),
-        'start_time' => $faker->time,
-        'end_time' => $faker->time,
+        'start_time' => date('H:i', rand(3900, 42000)),
+        'end_time' => date('H:i', rand(46800, 86100)),
         'phone_nr' => $faker->phoneNumber,
         'license_plate' => Str::random(9),
         'payed_at' => now(),
