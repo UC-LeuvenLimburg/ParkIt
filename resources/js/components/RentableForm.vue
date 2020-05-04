9b01d6c1402e79ad1ba2956ac0c30142fbab80e5
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-05-05 00:43:58 +0200

    added moment.js

--- resources/js/components/RentableForm.vue
+++ resources/js/components/RentableForm.vue
@@ -5,21 +5,20 @@
         </div>
         <div v-if="rentable" class="form-row">
             <div class="form-group col-md-4">
-                <label for="date">Date</label>
+                <label for="date">Available Date</label>
                 <input
                     type="date"
-                    :value="rentable.date_of_hire"
+                    :value="rentable.date_of_hire | formatDate"
                     class="form-control"
                     placeholder="Date"
                     readonly
                 />
             </div>
             <div class="form-group col-md-4">
-                <label for="start_time">Start Time</label>
+                <label for="start_time">Available Start Time</label>
                 <input
                     type="time"
                     id="start_time"
-                    name="start_time"
                     :value="rentable.start_time"
                     class="form-control"
                     placeholder="Start Time"
@@ -27,11 +26,10 @@
                 />
             </div>
             <div class="form-group col-md-4">
-                <label for="end_time">End Time</label>
+                <label for="end_time">Available End Time</label>
                 <input
                     type="time"
                     id="end_time"
-                    name="end_time"
                     :value="rentable.end_time"
                     class="form-control"
                     placeholder="Start Time"
@@ -44,6 +42,20 @@
 
 <script>
 import RentableAutocomplete from "./RentableAutocomplete";
+import moment from "moment";
+
+Vue.filter("formatDate", function(value) {
+    if (value) {
+        console.log(value);
+        return moment(String(value)).format("DD/MM/YYYY");
+    }
+});
+
+Vue.filter("formatTime", function(value) {
+    if (value) {
+        return moment(String(value)).format("HH:mm");
+    }
+});
 
 export default {
     data() {
