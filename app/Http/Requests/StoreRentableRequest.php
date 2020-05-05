61ecdd96b39f6a3e32a3f5ed7a88c4c60084c393
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-05-05 02:32:11 +0200

    added regexes for rentables

--- app/Http/Requests/StoreRentableRequest.php
+++ app/Http/Requests/StoreRentableRequest.php
@@ -7,6 +7,16 @@ use Illuminate\Foundation\Http\FormRequest;
 class StoreRentableRequest extends FormRequest
 {
     /**
+     * Sanitize before rules()
+     */
+    protected function sanitizeInput()
+    {
+        $input = $this->all();
+        $input['description'] = preg_replace("~[\p{M}]~uis", "", $this->input('description'));
+        $this->replace($input);
+    }
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
@@ -17,12 +27,12 @@ class StoreRentableRequest extends FormRequest
 
         return [
             'user_id' => 'required|integer|min:1|exists:App\Models\User,id',
-            'adress' => 'required|string|min:3|max:150',
+            'adress' => 'required|string|min:3|max:150|regex:/^[a-zA-Z0-9_ .-]*$/', // Regex for Adress
             'postal_code' => 'required|numeric|digits:4|min:1|max:9999',
             'date_of_hire' => 'required|date_format:Y-m-d|after_or_equal:' . $todayDate,
             'start_time' => 'required',
             'end_time' => 'required',
-            'price' => 'required|numeric|min:0.01|max:1000',
+            'price' => 'required|numeric|min:0.01|max:1000|regex:/^[0-9]+(\.[0-9]{1,2})?$/', //Regex for Decimal with 2 decimal places
             'bankaccount_nr' => 'required|string|regex:/^[A-Z]{2}(?:[ ]?[0-9]){14,20}$/', // Regex for IBAN numbers
             'description' => 'required|string|max:150',
         ];
@@ -35,6 +45,8 @@ class StoreRentableRequest extends FormRequest
      */
     protected function getValidatorInstance()
     {
+        $this->sanitizeInput();
+
         return parent::getValidatorInstance()->after(function () {
             // convert to unix timestamps
             $start_time = $this->input('start_time');
