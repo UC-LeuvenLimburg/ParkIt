7fc610b9eeba1429da0d15114953c8fe4ae1d316
Kenny-Lacroix <62295602+Kenny-Lacroix@users.noreply.github.com>
2020-05-05 22:59:52 +0200

    finished redirect page

--- resources/views/payment/redirect.blade.php
+++ resources/views/payment/redirect.blade.php
@@ -6,24 +6,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-xl">
-            @php
-
-            @endphp
-            <h4>Please wait a moment. You will be redirected in <span id="countdowntimer">5 </span> Seconds</h4>
-            <p>Wan't to go to the page now? Click <a href="/myleases"> here </a></p>
-        </div>
+        <payment-redirect></payment-redirect>
     </div>
 </div>
 @endsection
-
-<script type="text/javascript">
-    var timeleft = 5;
-    var timer = setInterval(function(){
-    if(timeleft <=0){
-        clearInterval(downloadTimer);
-    }
-    timeleft--;
-    document.getElementById("countdowntimer").textContent = timeleft;
-    },1000);
-</script>
