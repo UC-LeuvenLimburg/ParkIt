022cf9e89afedb09e2be0d342a8aeb601446b848
Kenny-Lacroix <62295602+Kenny-Lacroix@users.noreply.github.com>
2020-05-06 01:09:11 +0200

    changed faker location to belgium

--- database/factories/leaseFactory.php
+++ database/factories/leaseFactory.php
@@ -7,9 +7,10 @@ use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
 $factory->define(Lease::class, function (Faker $faker) {
+    $faker = \Faker\Factory::create('nl_BE');
     return [
         'user_id' => $faker->numberBetween(1, 50),
-        'rentable_id' => $faker->numberBetween(1, 50),
+        'rentable_id' => $faker->numberBetween(5, 50),
         'start_time' => date('H:i', rand(3900, 42000)),
         'end_time' => date('H:i', rand(46800, 86100)),
         'phone_nr' => $faker->phoneNumber,
