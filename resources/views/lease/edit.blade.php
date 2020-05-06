845517794814d49612a764ef619d0e4333a80246
kaat schillemans <kaatschillemans@hotmail.com>
2020-05-07 01:09:05 +0200

    changed lease end time factoring

--- resources/views/lease/edit.blade.php
+++ resources/views/lease/edit.blade.php
@@ -25,7 +25,7 @@
                 </div>
                 <div class="form-group col-md-4">
                     {{Form::label('end_time', 'End Time')}}
-                    {{Form::time('end_time', $lease->end_time, ['class' => 'form-control', 'placeholer' => 'End Time', 'required']) }}
+                    {{Form::time('end_time', date("H:i", strtotime($lease->end_time)), ['class' => 'form-control', 'placeholer' => 'End Time', 'required']) }}
                 </div>
                 @else
                 <div class="form-group col-md-4">
