e1561c3f4c40eea2149ff6a1fcddbd7447c77d17
kaat schillemans <kaatschillemans@hotmail.com>
2020-05-07 01:09:39 +0200

    changed lease start time

--- resources/views/lease/edit.blade.php
+++ resources/views/lease/edit.blade.php
@@ -21,7 +21,7 @@
                 @if (Auth::user()->role==="admin")
                 <div class="form-group col-md-4">
                     {{Form::label('start_time', 'Start Time')}}
-                    {{Form::time('start_time', $lease->start_time, ['class' => 'form-control', 'placeholer' => 'Start Time', 'required']) }}
+                    {{Form::time('start_time', date("H:i", strtotime($lease->start_time)), ['class' => 'form-control', 'placeholer' => 'Start Time', 'required']) }}
                 </div>
                 <div class="form-group col-md-4">
                     {{Form::label('end_time', 'End Time')}}
