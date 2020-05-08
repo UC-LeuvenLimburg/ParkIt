c80a84103221e6c956f97098510a6726f1830ddc
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-05-08 22:15:29 +0200

    created heremap search bar

---
+++ resources/js/components/HereMapSearchbar.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="searchbar">
+        <div class="form-row">
+            <div class="form-group col-md-4">
+                <label for="adress_filter">Adress Filter</label>
+                <input
+                    type="text"
+                    name="adress_filter"
+                    id="adress_filter"
+                    v-model="adress_filter"
+                    class="form-control"
+                    placeholer="Adress"
+                />
+            </div>
+            <div class="form-group col-md-4">
+                <label for="postal_code_filter">Postal code Filter</label>
+                <input
+                    type="text"
+                    name="postal_code_filter"
+                    id="postal_code_filter"
+                    v-model="postal_code_filter"
+                    class="form-control"
+                    placeholer="Postal code"
+                />
+            </div>
+            <div>
+                <div @click="search" class="search-btn ml-1 btn btn-primary">
+                    Search
+                </div>
+                <div @click="clear" class="search-btn ml-2 btn btn-warning">
+                    Clear
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
