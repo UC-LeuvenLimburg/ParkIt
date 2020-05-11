2f176c6b488a17a147a9fdf61869e38949420b8b
Kenny-Lacroix <62295602+Kenny-Lacroix@users.noreply.github.com>
2020-05-11 20:34:58 +0200

    added a way for users to pay

--- resources/views/lease/table.blade.php
+++ resources/views/lease/table.blade.php
@@ -23,7 +23,10 @@
             <td>
                 <a class="btn btn-info btn-sm" href='/leases/{{ $lease->id }}'>Show</a>
                 @if (Auth::user()->role==="admin" || $lease->user_id == Auth::id())
-                <a class="btn btn-info btn-sm btn-warning" href='/leases/{{ $lease->id }}/edit'>Edit</a>
+                <a class="btn btn-sm btn-warning" href='/leases/{{ $lease->id }}/edit'>Edit</a>
+                @endif
+                @if ( $lease->payed_at === NULL && $lease->user_id == Auth::id())
+                <a class="btn btn-danger btn-sm" href='/pay/{{$lease->id}}/'>Pay</a>
                 @endif
             </td>
         </tr>
