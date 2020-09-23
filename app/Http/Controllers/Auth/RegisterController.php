324edd71b6405aef0e1a0e0ddd097d341425cc43
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-09-23 18:15:59 +0200

    change two

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -30,7 +30,7 @@ class RegisterController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
-    // change
+    // change number two
 
     /**
      * Create a new controller instance.
